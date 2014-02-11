ce27e79d43ae2cf406fd561e78064e13265f1b57
tsun4 <thedanteshow@hotmail.fr>
2014-02-11 11:36:46 +0100

    Initial Commit

---
+++ TodoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  TodoList
+//
+//  Created by Ilan Hagege on 11/02/2014.
+//  Copyright (c) 2014 Ilan Hagege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoListTests/TodoListTests.m
@@ -0,0 +1,34 @@
+//
+//  TodoListTests.m
+//  TodoListTests
+//
+//  Created by Ilan Hagege on 11/02/2014.
+//  Copyright (c) 2014 Ilan Hagege. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TodoListTests : XCTestCase
+
+@end
+
+@implementation TodoListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
